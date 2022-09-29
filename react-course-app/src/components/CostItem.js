079c5b887b444da03fbcc3ff759aa339227dfc98
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-09-29 21:26:20 +0300

    Add CSS

--- react-course-app/src/components/CostItem.js
+++ react-course-app/src/components/CostItem.js
@@ -1,5 +1,15 @@
+import "./CostItem.css";
+
 function CostItem() {
-  return <h2>Cost Element</h2>;
+  return (
+    <div className="cost-item">
+      <div>Март 2021 12</div>
+      <div className="cost-item__description">
+        <h2>Телевизор</h2>
+        <div className="cost-item__price">400$</div>
+      </div>
+    </div>
+  );
 }
 
 export default CostItem;
