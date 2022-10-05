d7a96067f24a3736b086c6df6fcdcbfd2f2a9250
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-10-05 07:00:37 +0300

    Using lists with state

--- react-course-app/src/App.js
+++ react-course-app/src/App.js
@@ -1,31 +1,36 @@
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import React, { useState } from "react";
 
+const Initial_costs = [
+  {
+    id: "c1",
+    date: new Date(2021, 2, 12),
+    description: "Телевизор",
+    amount: 400.99,
+  },
+  {
+    id: "c2",
+    date: new Date(2021, 5, 10),
+    description: "Телефон",
+    amount: 235.99,
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 11, 8),
+    description: "Пылесос",
+    amount: 150.99,
+  },
+];
 const App = () => {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2021, 2, 12),
-      description: "Телевизор",
-      amount: 400.99,
-    },
-    {
-      id: "c2",
-      date: new Date(2021, 5, 10),
-      description: "Телефон",
-      amount: 235.99,
-    },
-    {
-      id: "c3",
-      date: new Date(2021, 11, 8),
-      description: "Пылесос",
-      amount: 150.99,
-    },
-  ];
+  const [costs, setCosts] = useState(Initial_costs);
 
   const addCostHandler = (cost) => {
-    console.log(cost);
-    console.log("App component");
+    //  setCosts([cost, ...costs ]) //sprad operator (..." ")
+    setCosts((prevCosts) => {
+      // previos state of "Costs"
+      return [cost, ...prevCosts];
+    });
   };
 
   return (
