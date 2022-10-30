c66e51f905128f836e90bbb65970adc10578850d
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-10-30 20:00:41 +0200

    Finished App

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+import CreateUser from "./components/Users/CreateUser";
+import UserList from "./components/Users/UserList";
+
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const createUserHandler = (name, age) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: name, age: age, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <h1>My self first React project!</h1>
+      <CreateUser onCreateUser={createUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
