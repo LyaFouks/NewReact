d4c88e1bbea2efb6ab89b0209256cd212d4ca777
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-11-09 07:47:24 +0200

    Dynamic Context

--- effects-reducers-context-project/src/components/MainHeader/Navigation.js
+++ effects-reducers-context-project/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from "../../store/auth-context";
 
 import styles from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
   return (
     <nav className={styles.nav}>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Выйти</button>
+            <button onClick={ctx.onLogout}>Выйти</button>
           </li>
         )}
       </ul>
