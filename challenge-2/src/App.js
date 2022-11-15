8d71ad77df1e9d07c3bf6d708249d626019384a1
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-11-15 07:24:17 +0200

    Dishes view

--- challenge-2/src/App.js
+++ challenge-2/src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layaout/Header";
 import React from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
