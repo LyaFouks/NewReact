07b56322ac5200715fa2754759f537ac9751544e
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-12-09 09:36:51 +0200

    Сomponent update

--- react-under-the-hood/src/App.js
+++ react-under-the-hood/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { useState } from "react";
+import Button from "./components/UI/Button";
 
 function App() {
+  const [isParagraphShown, setIsParagraphShown] = useState(false);
+
+  console.log("Hello");
+
+  const toogleParagraph = () => {
+    setIsParagraphShown((prevIsParagraphShown) => !prevIsParagraphShown);
+  };
+
   return (
     <div className="app">
       <h1>React под Капотом</h1>
+      {isParagraphShown && <p>New paragraph</p>}
+      <Button onClick={toogleParagraph}>Show / Hide</Button>
     </div>
   );
 }
