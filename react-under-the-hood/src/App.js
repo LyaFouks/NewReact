7bf5b6eb70c52040e6f44adb3050698c30c217c1
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-12-21 18:26:49 +0200

    useCallback dependencies

--- react-under-the-hood/src/App.js
+++ react-under-the-hood/src/App.js
@@ -6,16 +6,25 @@ import ParagraphOutput from "./components/UI/Output/ParagraphOutput";
 function App() {
   const [isParagraphShown, setIsParagraphShown] = useState(false);
 
+  const [isToggleActivated, setIsToggleActivated] = useState(false);
+
   console.log("App component");
 
   const toogleParagraph = useCallback(() => {
-    setIsParagraphShown((prevIsParagraphShown) => !prevIsParagraphShown);
-  }, []);
+    if (isToggleActivated) {
+      setIsParagraphShown((prevIsParagraphShown) => !prevIsParagraphShown);
+    }
+  }, [isToggleActivated]);
+
+  const activatedToggling = () => {
+    setIsToggleActivated(true);
+  };
 
   return (
     <div className="app">
       <h1>React под Капотом</h1>
       <ParagraphOutput isShown={isParagraphShown} />
+      <Button onClick={activatedToggling}>Activated toggle</Button>
       <Button onClick={toogleParagraph}>Show / Hide</Button>
     </div>
   );
